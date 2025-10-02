a3cee3d13e6624f6287bab699ae5f9d4edf02210
Razvan Tabacariu <160879003+RazvanTabacariu@users.noreply.github.com>
2025-10-02 13:45:44 +0200

    Dockerfile paths
    Updated paths in Dockerfile and improved directory handling.

--- cicd-sample-app/sample-app.sh
+++ cicd-sample-app/sample-app.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 set -euo pipefail
+
 cd cicd-sample-app
+
 rm -rf tempdir
-mkdir -p tempdir
+mkdir tempdir
 mkdir -p tempdir/templates
 mkdir -p tempdir/static
 
 cp sample_app.py tempdir/.
-cp -r templates/* tempdir/templates/
-cp -r static/*    tempdir/static/    
+cp -r templates/. tempdir/templates/
+cp -r static/.    tempdir/static/    
 
 
 cat > tempdir/Dockerfile << _EOF_
 FROM python
 RUN pip install flask
-COPY  cicd-sample-app/cicd-sample-app/static /home/myapp/static/
-COPY  cicd-sample-app/cicd-sample-app/templates /home/myapp/templates/
-COPY  cicd-sample-app/cicd-sample-app/sample_app.py /home/myapp/
+COPY  static /home/myapp/static/
+COPY  templates /home/myapp/templates/
+COPY  sample_app.py /home/myapp/
 EXPOSE 5050
 CMD python /home/myapp/sample_app.py
 _EOF_
